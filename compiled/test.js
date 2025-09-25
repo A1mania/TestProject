e2cacc1bb49a1f9303b689b12849910b8a27a57f
Anna Pachkovskaya <106625221+A1mania@users.noreply.github.com>
2025-09-25 17:50:03 +0300

    HW3 done

---
+++ compiled/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var test = 1;
+var test2 = 2;
+var sum = test + test2;
+console.log(sum);
+//# sourceMappingURL=test.js.map
---
+++ compiled/test2.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var var1 = -1;
+var var2 = -0.5;
+var operator = "*";
+var result;
+if (operator == "*") {
+    result = var1 * var2;
+}
+else if (operator == "/") {
+    result = var1 / var2;
+}
+else if (operator == "+") {
+    result = var1 + var2;
+}
+else if (operator == "-") {
+    result = var1 - var2;
+}
+else {
+    result = "Invalid operator!";
+}
+console.log(result);
+//# sourceMappingURL=test2.js.map
